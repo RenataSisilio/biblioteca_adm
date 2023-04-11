3db95a8c598c3ce4488f336e63bfd96436cc20bf
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2023-04-11 16:17:53 -0300

    fix: user history search

--- lib/routes/history/user_history.dart/user_history_controller.dart
+++ lib/routes/history/user_history.dart/user_history_controller.dart
@@ -11,14 +11,13 @@ class UserHistoryController extends Cubit<UserHistoryState> {
   UserHistoryController(this.repository) : super(UserHistoryState.loading);
 
   final LibraryRepository repository;
-  late List<Move> history;
   late List<Move> borrowed;
   late List<Move> returned;
 
   Future<void> getHistory(String user) async {
     emit(UserHistoryState.loading);
     try {
-      history = await repository.getMoves(user);
+      final history = await repository.getMoves(user);
       history.sort((a, b) => b.type.index.compareTo(a.type.index));
       final firstIndex =
           history.indexWhere((move) => move.type == Status.borrowed);
@@ -33,15 +32,15 @@ class UserHistoryController extends Cubit<UserHistoryState> {
   }
 
   String getDates(int index) {
-    final subListIndex = returned.indexWhere(
-      (move) => move.date.isBefore(borrowed[index].date),
-    );
-    final subList = returned.sublist(
-      0,
-      subListIndex == -1 ? 0 : subListIndex,
-    );
+    // final subListIndex = returned.indexWhere(
+    //   (move) => move.date.isBefore(borrowed[index].date),
+    // );
+    // final subList = returned.sublist(
+    //   0,
+    //   subListIndex == -1 ? 0 : subListIndex,
+    // );
     final returnIndex =
-        subList.indexWhere((move) => move.bookId == borrowed[index].bookId);
+        returned.indexWhere((move) => move.bookId == borrowed[index].bookId);
     return DateFormat('dd/MM/yy').format(borrowed[index].date) +
         (returnIndex == -1
             ? ''
